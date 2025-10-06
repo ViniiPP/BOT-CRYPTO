1f880d9a44d9b177a29f1663cafacbb1e056c0ba
ViniiPP <viniciusppolli07@gmail.com>
2025-10-06 20:40:07 -0300

    DEV

---
+++ index.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+const SYMBOL = 'BTCUSDT';
+const DAY_PRICE = 34160;
+const SELL_PRICE = 34501;
+const API_URL = 'https://testnet.binance.vision'; // muda dps pela API nativa
+
+let IsOpened = false;
+
+async function start() {
+    const {data} = await axios.get(API_URL + `/api/v3/klines?limit=21&interval=15m&symbol=${SYMBOL}`);
+    const candle =data[data.length -1];
+    const price = parseFloat(candle[4]);
+
+    console.clear();
+    console.log('Price: ' + price);
+
+    if (price >= DAY_PRICE && IsOpened == false) {
+        console.log('Comprar');
+        IsOpened = true;
+    } else if (price >= SELL_PRICE && IsOpened == true) {
+        console.log('Vender');
+        IsOpened = false;
+    } else {
+        console.log('Aguardar');
+    }
+}
+setInterval(start, 3000);
+
+start();
