1cd898b2303e60479272070cdfbcb827bb0a79e4
ViniiPP <viniciusppolli07@gmail.com>
2025-10-07 15:30:39 -0300

    feat: Adiciona função para negociação

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const axios = require('axios');
 const SYMBOL = 'BTCUSDT';
 const DAY_PRICE = 34160;
 const SELL_PRICE = 34501;
-const API_URL = 'https://testnet.binance.vision'; // muda dps pela API nativa
+
+const API_URL = 'https://testnet.binance.vision'; // mudar dps pela API nativa https://api.binance.com
 
 let IsOpened = false;
+
+function calcSMA(data) {
+    const closes = data.map(candle => parseFloat(candle[4]));   
+    const sum = closes.reduce((a,b) => a + b);
+    return sum / data.length;
+}
 
 async function start() {
     const {data} = await axios.get(API_URL + `/api/v3/klines?limit=21&interval=15m&symbol=${SYMBOL}`);
@@ -14,6 +21,10 @@ async function start() {
 
     console.clear();
     console.log('Price: ' + price);
+
+    const sma = calcSMA(data);
+    console.log('SMA: ' + sma);
+    console.log('Is Opened: ' + IsOpened);
 
     if (price >= DAY_PRICE && IsOpened == false) {
         console.log('Comprar');
